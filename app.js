338d0fbbb7cf2a5379db0304fa285e023bc8e1e0
Samet Can Gözüaçık <sametcanngozuacik@gmail.com>
2024-03-05 11:04:59 +0300

    Click added

--- app.js
+++ app.js
@@ -27,47 +27,47 @@ function applyNote() {
     const noteInput = document.getElementById("noteInput");
     const note = noteInput.value.trim();
     if (note !== "") {
-        // Yeni notu HTML içeriğine ekle
+
         const notesContainer = document.getElementById("notesContainer");
         const newNoteDiv = document.createElement("div");
         newNoteDiv.classList.add("note-item");
 
-        // Checkbox oluştur
+
         const checkbox = document.createElement("input");
         checkbox.type = "checkbox";
         checkbox.classList.add("note-checkbox");
         newNoteDiv.appendChild(checkbox);
 
-        // Not metni oluştur
+
         const noteText = document.createElement("span");
         noteText.textContent = note;
         newNoteDiv.appendChild(noteText);
 
-        // Silme butonu ekle
+
         const deleteButton = document.createElement("img");
-        deleteButton.src = "images/delete-button.svg"; // Silme butonunun resminin yolu
-        deleteButton.alt = "Delete"; // Silme butonunun alternatif metni
+        deleteButton.src = "images/delete-button.svg";
+        deleteButton.alt = "Delete";
         deleteButton.classList.add("delete-button");
-        deleteButton.style.cursor = "pointer"; // Fare işaretçisini bir el işareti yapar
+        deleteButton.style.cursor = "pointer";
         deleteButton.addEventListener("click", function () {
-            // Silme butonuna tıklandığında notu sil
+
             newNoteDiv.remove();
         });
         newNoteDiv.appendChild(deleteButton);
 
         notesContainer.appendChild(newNoteDiv);
 
-        // Her notun altına çizgi ekle
+
         if (notesContainer.children.length > 1) {
             const previousNote = notesContainer.children[notesContainer.children.length - 2];
             previousNote.style.borderBottom = "1px solid #ccc";
             previousNote.style.marginBottom = "10px";
         }
 
-        // Not giriş kutusunu temizle
+
         noteInput.value = "";
 
-        // Not giriş kutusunu gizle
+
         const noteContainer = document.getElementById("noteContainer");
         noteContainer.style.display = "none";
     } else {
